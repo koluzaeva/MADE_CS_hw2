2d530c792ca2603f201db35f7d81b2d09e4ad0cf
kate_kosareva <koluzaeva@gmail.com>
2022-10-15 10:25:21 +0200

    fit and predict added

--- model.py
+++ model.py
@@ -2,4 +2,12 @@ class Model:
 	def __init__(self, name, model):
 		self.model = model
 		self.name = name
+	
+	 def fit(self, X_train, y_treain):
+                self.fit(X_train, y_train)
+
+        def predict(self, X_test):
+                self.predict = self.model.predict(X_test)
+                return self.pred
+
 
